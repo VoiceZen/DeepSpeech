a4c32d1db494076b107b1a970b3443161eef49c0
Your Name <you@example.com>
2019-02-11 07:34:54 +0530

    changes in gpu box

--- greed_infer.py
+++ greed_infer.py
@@ -1,55 +1,40 @@
-"""Inferer for DeepSpeech2 model."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+from multiprocessing import Process
 import argparse
 import functools
-import paddle.v2 as paddle
-from data_utils.data import DataGenerator
-from model_utils.model import DeepSpeech2Model
-from utils.utility import add_arguments, print_arguments
-from util import infer_batch
-import pandas as pd
 
 from vz.asr.base.encoders import ProbCodec
+
+from data_utils.data import DataGenerator
+from utils.utility import add_arguments
+from util import infer_batch
+
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
 # yapf: disable
-add_arg('num_samples',      int,    10,     "# of samples to infer.")
-add_arg('trainer_count',    int,    8,      "# of Trainers (CPUs or GPUs).")
-add_arg('beam_size',        int,    500,    "Beam search width.")
-add_arg('num_proc_bsearch', int,    8,      "# of CPUs for beam search.")
-add_arg('num_conv_layers',  int,    2,      "# of convolution layers.")
-add_arg('num_rnn_layers',   int,    3,      "# of recurrent layers.")
-add_arg('rnn_layer_size',   int,    2048,   "# of recurrent cells per layer.")
-add_arg('use_gru',          bool,   False,  "Use GRUs instead of simple RNNs.")
-add_arg('use_gpu',          bool,   True,   "Use GPU or not.")
-add_arg('share_rnn_weights',bool,   True,   "Share input-hidden weights across "
-                                            "bi-directional RNNs. Not for GRU.")
-add_arg('infer_manifest',   str,
+add_arg('num_samples', int, 10, "# of samples to infer.")
+add_arg('trainer_count', int, 8, "# of Trainers (CPUs or GPUs).")
+add_arg('num_conv_layers', int, 2, "# of convolution layers.")
+add_arg('num_rnn_layers', int, 3, "# of recurrent layers.")
+add_arg('rnn_layer_size', int, 2048, "# of recurrent cells per layer.")
+add_arg('use_gru', bool, False, "Use GRUs instead of simple RNNs.")
+add_arg('use_gpu', bool, True, "Use GPU or not.")
+add_arg('share_rnn_weights', bool, True, "Share input-hidden weights across "
+                                         "bi-directional RNNs. Not for GRU.")
+add_arg('infer_manifest', str,
         'data/librispeech/manifest.dev-clean',
         "Filepath of manifest to infer.")
-add_arg('mean_std_path',    str,
+add_arg('mean_std_path', str,
         'data/librispeech/mean_std.npz',
         "Filepath of normalizer's mean & std.")
-add_arg('vocab_path',       str,
+add_arg('vocab_path', str,
         'data/librispeech/vocab.txt',
         "Filepath of vocabulary.")
-add_arg('model_path',       str,
+add_arg('model_path', str,
         './checkpoints/libri/params.latest.tar.gz',
         "If None, the training starts from scratch, "
         "otherwise, it resumes from the pre-trained model.")
-add_arg('decoding_method',  str,
-        'ctc_beam_search',
-        "Decoding method. Options: ctc_beam_search, ctc_greedy",
-        choices = ['ctc_beam_search', 'ctc_greedy'])
-add_arg('error_rate_type',  str,
-        'wer',
-        "Error rate type for evaluation.",
-        choices=['wer', 'cer'])
-add_arg('specgram_type',    str,
+add_arg('specgram_type', str,
         'linear',
         "Audio feature type. Options: linear, mfcc.",
         choices=['linear', 'mfcc'])
@@ -57,8 +42,10 @@ add_arg('specgram_type',    str,
 args = parser.parse_args()
 
 
-def infer():
-    """Inference for DeepSpeech2."""
+def print_func(continent='Asia'):
+    print('The name of continent is : ', continent)
+
+if __name__ == "__main__":
     data_generator = DataGenerator(
         vocab_filepath=args.vocab_path,
         mean_std_filepath=args.mean_std_path,
@@ -73,59 +60,13 @@ def infer():
         sortagrad=False,
         shuffle_method=None)
 
-    ds2_model = DeepSpeech2Model(
-        vocab_size=data_generator.vocab_size,
-        num_conv_layers=args.num_conv_layers,
-        num_rnn_layers=args.num_rnn_layers,
-        rnn_layer_size=args.rnn_layer_size,
-        use_gru=args.use_gru,
-        pretrained_model_path=args.model_path,
-        share_rnn_weights=args.share_rnn_weights)
-
     vocab_list = [chars.encode("utf-8") for chars in data_generator.vocab_list]
     codec = ProbCodec(vocab_list, True)
-    iterations_for_write = 2  # 200
-    res = []
     cols = ['filepath', 'orig_script', 'infer_script', 'encode']
+
     for batch_id, data_batch in enumerate(batch_reader()):
-        print("batch id is {}".format(batch_id))
-        print((batch_id + 1) % iterations_for_write)
-        if (batch_id + 1) % iterations_for_write == 0:
-            # write to csv
-            # this is just to ensure oom
-            df = pd.DataFrame(res, columns=cols)
-            tmp = int((batch_id + 1) / iterations_for_write)
-            df.to_csv(
-                'batch-infer-{}.csv'.format(tmp),
-                index=False,
-                header=False
-            )
-            res = []
+        infer_args = (data_batch, data_generator, codec, batch_id, args)
+        proc = Process(target=infer_batch, args=infer_args)
+        proc.start()
+        proc.join()
 
-        infer_out = infer_batch(
-            ds2_model, data_batch, data_generator, codec, args)
-        res.extend(infer_out)
-
-    df = pd.DataFrame(res, columns=cols)
-    df.to_csv(
-        'batch-infer-last.csv'.format(batch_id),
-        index=False,
-        header=False
-    )
-    import pdb; pdb.set_trace()  # breakpoint 57d9f2d3 //
-
-
-
-def main():
-    print_arguments(args)
-    if args.use_gpu:
-        paddle.init(
-            use_gpu=True,
-            rnn_use_batch=True,
-            trainer_count=args.trainer_count)
-    else:
-        paddle.init(use_gpu=False, trainer_count=1)
-    infer()
-
-if __name__ == '__main__':
-    main()
--- util.py
+++ util.py
@@ -1,4 +1,31 @@
-def infer_batch(ds2_model, infer_data, data_generator, codec, args):
+import time
+import pandas as pd
+import paddle.v2 as paddle
+
+from model_utils.model import DeepSpeech2Model
+
+
+def infer_batch(infer_data, data_generator, codec, batch_id, args):
+    st1 = time.time()
+    if args.use_gpu:
+        paddle.init(
+            use_gpu=True,
+            rnn_use_batch=True,
+            trainer_count=args.trainer_count)
+    else:
+        paddle.init(use_gpu=False, trainer_count=1)
+
+    ds2_model = DeepSpeech2Model(
+        vocab_size=data_generator.vocab_size,
+        num_conv_layers=args.num_conv_layers,
+        num_rnn_layers=args.num_rnn_layers,
+        rnn_layer_size=args.rnn_layer_size,
+        use_gru=args.use_gru,
+        pretrained_model_path=args.model_path,
+        share_rnn_weights=args.share_rnn_weights)
+
+    print("Processing batch {}".format(batch_id))
+    st2 = time.time()
     # decoders only accept string encoded in utf-8
     vocab_list = [chars.encode("utf-8") for chars in data_generator.vocab_list]
 
@@ -30,4 +57,19 @@ def infer_batch(ds2_model, infer_data, data_generator, codec, args):
             encode
         ])
 
+    cols = ['filepath', 'orig_script', 'infer_script', 'encode']
+    df = pd.DataFrame(res, columns=cols)
+    df.to_csv(
+        'batch-infer-{}.csv'.format(batch_id + 1),
+        index=False,
+        header=False
+    )
+    et = time.time()
+
+    total_time1 = (et - st1)
+    total_time2 = (et - st2)
+    duration = 10
+    print("Time taken to process batch of duration {} is {}-{}".format(
+        duration, total_time1, total_time2))
     return res
+
